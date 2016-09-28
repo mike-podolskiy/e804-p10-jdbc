1853e430807e9b9c2fa86a8fadf9b7c750bb28c9
Михаил Подольский <podolskiy.mikhail@inessoft.kz>
2016-09-28 10:00:20 +0600

    JDBC - JoinRowSet пример

--- src/main/java/kz/mix/e804/jdbc/main/rowSet/join/JoinRowSetTest.java
+++ src/main/java/kz/mix/e804/jdbc/main/rowSet/join/JoinRowSetTest.java
@@ -1,36 +1,36 @@
 //CHECKSTYLE:OFF
-package kz.mix.e804.jdbc.main.rowSet;
+package kz.mix.e804.jdbc.main.rowSet.join;
 
 import javax.sql.RowSet;
 import javax.sql.rowset.CachedRowSet;
 import javax.sql.rowset.JoinRowSet;
 import javax.sql.rowset.RowSetFactory;
 import javax.sql.rowset.RowSetProvider;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 // Пример JoinRowSet
-public class Learning {
+public class JoinRowSetTest {
 
     public static void main(String[] args) throws Exception {
         String url = "jdbc:mysql://localhost:3306/learning";
         String userName = "root";
         String password = "welcome";
 
+        Connection connection = DriverManager.getConnection(url, userName, password);
         RowSetFactory rsf = RowSetProvider.newFactory();
 
         CachedRowSet studentRS = rsf.createCachedRowSet();
-        init(studentRS, url, userName, password);
         studentRS.setCommand("select sid, name from student");
-        studentRS.execute();
+        studentRS.execute(connection);
 
         CachedRowSet enrollmentRS = rsf.createCachedRowSet();
-        init(enrollmentRS, url, userName, password);
         enrollmentRS.setCommand("select sid, subject from enrollment");
-        enrollmentRS.execute();
+        enrollmentRS.execute(connection);
 
         CachedRowSet teacherRS = rsf.createCachedRowSet();
-        init(teacherRS, url, userName, password);
         teacherRS.setCommand("select tid, subject from teacher");
-        teacherRS.execute();
+        teacherRS.execute(connection);
 
         JoinRowSet jrs = rsf.createJoinRowSet();
         jrs.addRowSet(studentRS, "sid");
@@ -41,17 +41,13 @@ public class Learning {
         jrs2.addRowSet(jrs);
         jrs2.addRowSet(teacherRS, "subject");
 
-        System.out.println(jrs2.getMetaData().getColumnCount());
+        int colNumber = jrs2.getMetaData().getColumnCount();
 
         while (jrs2.next()) {
-            System.out.println(jrs2.getObject(1) + " \t" + jrs2.getObject(2) + " \t" + jrs2.getObject(3) + " \t" + jrs2.getObject(4));
+            for (int i = 1; i < colNumber; i++) {
+                System.out.print(jrs2.getObject(i) + " \t");
+            }
+            System.out.println();
         }
-
-    }
-
-    private static void init(RowSet rs, String url, String userName, String password) throws Exception {
-        rs.setUrl(url);
-        rs.setUsername(userName);
-        rs.setPassword(password);
     }
 }
